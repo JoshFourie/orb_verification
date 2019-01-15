bfca32785bbce91db1cbe90488631f146a8b500d
JoshFourie <josh@omniaprotocol.com>
2019-01-15 15:29:03 +1100

    info! macro for temporarily logging proofs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use lambda_runtime::{lambda, Context, error::HandlerError};
-use log::error;
+use log::{info, error};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     simple_logger::init_with_level(log::Level::Debug)
@@ -13,7 +13,18 @@ fn verification_handler(object: String, ctx: Context) -> Result<bool, HandlerErr
 
     match serde_json::from_str::<BackPack<CommonReference<FrLocal, G1Local, G2Local>, FrLocal, G1Local, G2Local, GtLocal>>(&object) 
     {
-        Ok(orb) => Ok(orb.verify()),
+        Ok(orb) => {
+            match orb.verify() {
+                true => {
+                    info!("True [PLACEHOLDER PUBLIC_KEY]");
+                    Ok(true)
+                },
+                false => {
+                    info!("False [PLACEHOLDER PUBLIC_KEY]");
+                    Ok(false)
+                },
+            }
+        },
         Err(_) => {
             error!("orb_verification::verification_handler::from_str() panicked when deserializing the object");
             Err(ctx.new_error("verification_handler::from_str() panicked when deserializing the object"))
